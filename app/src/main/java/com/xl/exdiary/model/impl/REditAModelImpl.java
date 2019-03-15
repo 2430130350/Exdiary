5971d305fd6befd506d8d43b70eea94b3176f01b
2430130350 <498478105@qq.com>
2019-03-15 13:03:16 +0800

    阅读编辑页面使用cardview实现、 背景高斯模糊完成、 从此处开始、需要编辑REditActivity的逻辑的、可以新建分支、然后拉取合并、

---
+++ app/src/main/java/com/xl/exdiary/model/impl/REditAModelImpl.java
@@ -0,0 +1,6 @@
+package com.xl.exdiary.model.impl;
+
+import com.xl.exdiary.model.inter.IREditAModel;
+
+public class REditAModelImpl implements IREditAModel {
+}
---
+++ app/src/main/java/com/xl/exdiary/view/activity/REditActivity.java
@@ -0,0 +1,128 @@
+package com.xl.exdiary.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.xl.exdiary.EXdiaryApplication;
+import com.xl.exdiary.R;
+import com.xl.exdiary.presenter.impl.REditAPresenterImpl;
+import com.xl.exdiary.presenter.inter.IREditAPresenter;
+import com.xl.exdiary.view.inter.IREditAView;
+/*---------------该类是无用类、仅为维持MVP架构存在、其内部实现已经在上一版本转移到MainActivity中的卡片不居中、-------------------*/
+public class REditActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, IREditAView {
+
+    private IREditAPresenter mIREditAPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mIREditAPresenter = new REditAPresenterImpl(this);
+        setContentView(R.layout.activity_redit);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //以下是自定义的操作、
+        EXdiaryApplication eXdiaryApplication = (EXdiaryApplication)this.getApplication();
+        Toast.makeText(this, eXdiaryApplication.str + "被点击而且正在显示中、", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.redit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_self) {//用户信息、
+            // Handle the camera action
+
+
+        } else if (id == R.id.nav_friend) {//好友日记、
+
+
+        } else if (id == R.id.nav_mine) {//我的日记、
+            startActivity(new Intent(this, MainActivity.class));
+            this.finish();
+
+        } else if (id == R.id.nav_noname) {//树洞、
+
+        } else if (id == R.id.nav_setting) {//设置、
+
+        } else if (id == R.id.nav_about) {//关于、
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public <T> T request(int requestFlag) {
+        return null;
+    }
+
+    @Override
+    public <T> void response(T response, int responseFlag) {
+
+    }
+}
