fc0b0d108614e5951231005313e4d2aa7950610b
shimonoaria <1292734753@qq.com>
2019-03-21 16:22:46 +0800

    feat:本地记录
    Signed-off-by: shimonoaria <1292734753@qq.com>

--- app/src/main/java/com/xl/exdiary/model/impl/User.java
+++ app/src/main/java/com/xl/exdiary/model/impl/User.java
@@ -2,12 +2,14 @@ package com.xl.exdiary.model.impl;
 
 public class User {
     private String name;
-    private int deviceNumber;
+    private String deviceNumber;
     private String signature;
-    public User(String name, int deviceNumber,String signature) {
+    private String mail;
+    public User(String name, String deviceNumber, String signature, String mail) {
         this.name = name;
         this.deviceNumber=deviceNumber;
         this.signature=signature;
+        this.mail = mail;
     }
 
     public String getName() {
@@ -26,12 +28,20 @@ public class User {
         this.signature = signature;
     }
 
-    public int getDeviceNumber() {
+    public String getDeviceNumber() {
         return deviceNumber;
     }
 
-    public void setDeviceNumber(int deviceNumber) {
+    public void setDeviceNumber(String deviceNumber) {
         this.deviceNumber = deviceNumber;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
     }
 }
 
