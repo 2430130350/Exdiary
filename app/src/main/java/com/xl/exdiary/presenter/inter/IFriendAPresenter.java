f39814b1ca4ea6e46f429215231738c2fb65351b
asus <yttandzt@163.com>
2019-04-09 18:21:31 +0800

    增加了好友操作，请求组合调试网络连接

--- app/src/main/java/com/xl/exdiary/presenter/inter/IFriendAPresenter.java
+++ app/src/main/java/com/xl/exdiary/presenter/inter/IFriendAPresenter.java
@@ -2,21 +2,117 @@ package com.xl.exdiary.presenter.inter;
 
 import com.xl.exdiary.model.impl.User;
 
-public interface IFriendAPresenter {
     /*
-    以下获得好友的增删改查
-     */
-    public User getFriend(String name, String uuid);//获得朋友信息
-    public User[] getAllFriend();//获得所有朋友信息
-    public boolean delFriends(String uuid);//删除朋友
-    public boolean modifyFriend(String name, String uuid, String mail, String signature);//修改用户信息
-    public boolean saveFriend(String name, String uuid, String mail, String signature);//保存好友信息
+     以下获得好友的增删改查
+    */
+public interface IFriendAPresenter {
 
     /*
-    以下完成好友添加和请求
+        获取指定好友信息
+
+        输入朋友的 name uuid
+        其中 name 可以为 null
+             uuid 不为空
+        返回朋友的 User 类的实例
+        包含 name deviceNumber signature   mail 信息
+
+        无好友信息则返回 null
+         暂未对异常做处理
      */
-    public User[] getFriendToSure();//返回所有的未添加的好友
-    public boolean acceptFriend(String fname, String fuuid, String mail, String signature);//接受好友请求
-    public boolean addFriends(String name, String uuid);//添加朋友
+    public User getFriend(String name, String uuid);
+
+    /*
+        获得所有好友信息
+
+        输入  无
+
+        输出 ： 有好友信息  User[]
+                无好友信息  null
+
+        异常 ： 暂无异常处理
+     */
+    public User[] getAllFriend();
+
+    /*
+        删除好友
+
+        输入 ： 好友 uuid
+
+        输出 ： 正确执行  true
+                错误执行  false
+
+        异常 ： 暂无异常处理
+     */
+    public boolean delFriends(String uuid);
+
+    /*
+        修改好友信息
+
+        输入 ： 好友信息  name uuid mail signature （uuid不可变，其余参数可谓 null）
+
+        输出 :  正确执行 true
+
+        错误执行： false
+
+        异常 ： 未对异常做处理
+     */
+    public boolean modifyFriend(String name, String uuid, String mail, String signature);
+
+    /*
+        保存好友信息
+
+        本地函数 不对外调用
+     */
+    public boolean saveFriend(String name, String uuid, String mail, String signature);
+
+
+    /*
+     以下完成好友添加和请求
+     */
+    /*
+        返回未添加的好友列表
+
+        输入 ：
+
+        输出 ： 返回 User[]
+
+        异常 ： 未对异常做处理
+     */
+    public User[] getFriendToSure();
+
+    /*
+        接受好友请求
+
+        输入 ： 好友信息 name  uuid mail signature (uuid不可为空，其余自定义）
+
+        输出 ： 正确执行 true
+                错误执行 false
+
+         异常 ：未对异常做处理
+     */
+    public boolean acceptFriend(String fname, String fuuid, String mail, String signature);
+
+    /*
+        添加朋友请求
+
+        输入 ： 好友信息  name  uuid
+
+        输出 ： 正确执行  true
+                错误执行  false
+
+         异常 ： 未对异常做处理
+     */
+    public boolean addFriends(String name, String uuid);
+
+    /*
+        拒绝好友请求
+
+        输入 : 好友 uuid
+
+        输出 ： 正确执行 true
+                错误执行 false
+
+         异常 ： 未对异常做处理
+     */
     public boolean rejectFriend(String fuuid);//拒绝好友请求
 }
